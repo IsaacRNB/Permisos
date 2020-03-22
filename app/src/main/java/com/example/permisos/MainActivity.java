b02bd2fa70a036f26f94dc07a8594805e1e87d2d
isaac bernal <ricardon1230@gmail.com>
2020-03-22 16:52:54 -0600

    update 4

--- app/src/main/java/com/example/permisos/MainActivity.java
+++ app/src/main/java/com/example/permisos/MainActivity.java
@@ -15,7 +15,11 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
-    private static final int STORAGE_PERMISSION_CODE = 1, LOCATION_PERMISSION_CODE = 2;
+    private static final int STORAGE_PERMISSION_CODE = 1,
+            LOCATION_PERMISSION_CODE = 2,
+            RECORD_PERMISSION_CODE = 3,
+            CALENDAR_PERMISSION_CODE = 4,
+            CAMERA_PERMISSION_CODE = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +37,19 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        Button buttonRequest2 = findViewById(R.id.switch2);
+        buttonRequest2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(MainActivity.this, "Ya tienes este permiso", Toast.LENGTH_LONG).show();
+                } else {
+                    requestLocationPermission();
+                }
+            }
+        });
+
     }
 
     private void requestStoragePermission() {
@@ -58,11 +75,37 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void requestLocationPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            new AlertDialog.Builder(this).setTitle("Permiso necesario").setMessage("..............").setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_CODE);
+                }
+            })
+                    .setNegativeButton("cancelar", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .create().show();
+
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_CODE);
+        }
+    }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode) {
             case STORAGE_PERMISSION_CODE:
             case LOCATION_PERMISSION_CODE:
+            case RECORD_PERMISSION_CODE:
+            case CALENDAR_PERMISSION_CODE:
+            case CAMERA_PERMISSION_CODE:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     Toast.makeText(this, "Permiso concedido", Toast.LENGTH_SHORT).show();
                 } else {
